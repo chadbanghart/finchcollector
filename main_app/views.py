1e8596ba270fd5d71591757d996f4b49e94a9a2c
chadbanghart <cbanghar@gmail.com>
2024-05-28 13:21:49 -0700

    part 1 mvp

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+finches = [
+  {'name': 'Flick', 'species': 'zebra finch', 'description': 'energetic and lively', 'age': 1},
+  {'name': 'Peep', 'species': 'society finch', 'description': 'sociable and friendly', 'age': 2},
+  {'name': 'Tweet', 'species': 'gouldian finch', 'description': 'colorful and vibrant', 'age': 3},
+  {'name': 'Chirp', 'species': 'star finch', 'description': 'calm and peaceful', 'age': 2},
+  {'name': 'Flutter', 'species': 'spice finch', 'description': 'curious and playful', 'age': 1}
+]
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
