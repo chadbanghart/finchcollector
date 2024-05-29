a7bd16290250d52b82d951cca8da32ee030f59f8
chadbanghart <cbanghar@gmail.com>
2024-05-29 10:57:16 -0700

    finch lab pt2

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
 
+from .models import Finch
+
 # Create your views here.
-finches = [
-  {'name': 'Flick', 'species': 'zebra finch', 'description': 'energetic and lively', 'age': 1},
-  {'name': 'Peep', 'species': 'society finch', 'description': 'sociable and friendly', 'age': 2},
-  {'name': 'Tweet', 'species': 'gouldian finch', 'description': 'colorful and vibrant', 'age': 3},
-  {'name': 'Chirp', 'species': 'star finch', 'description': 'calm and peaceful', 'age': 2},
-  {'name': 'Flutter', 'species': 'spice finch', 'description': 'curious and playful', 'age': 1}
-]
 
 def home(request):
   return render(request, 'home.html')
@@ -16,6 +11,11 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {
     'finches': finches
   })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
